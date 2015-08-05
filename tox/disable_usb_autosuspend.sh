e1dc2553a8ff3051e5d20d6934ab79d0531a5067
alemart <alemartf@gmail.com>
2015-08-05 03:16:48 -0300

    Enviando script que desabilita USB autosuspend...

--- tox/disable_usb_autosuspend.sh
+++ tox/disable_usb_autosuspend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# desabilita USB autosuspend (p/ o Kinect funcionar)
+# desabilita USB autosuspend
 for i in /sys/bus/usb/devices/*/power/autosuspend; do
     echo $i
     echo -1 | sudo tee $i
